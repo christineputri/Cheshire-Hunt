f3bc2cce5c34bbdea0743d37b625c88849dba484
christineputri <christineputri21@gmail.com>
2024-05-20 00:55:45 +0700

    Initial Commit

---
+++ Cheshire Hunt/Cheshire_HuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cheshire_HuntApp.swift
+//  Cheshire Hunt
+//
+//  Created by Christine Putri on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Cheshire_HuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
